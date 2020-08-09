3c0487b7b80d79a4b90bf9e2ebb5e2001cbba97f
richard4s <richardoluwo50@gmail.com>
2020-08-09 13:26:16 +0100

    Added auth pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="Content-Language" content="en">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>FCCPC COMPETITION PORTAL</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
-    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
-    <meta name="msapplication-tap-highlight" content="no">
-
-    <link href="{{ URL::asset('assets/css') }}/main.css" rel="stylesheet"></head>
-<body>
+@section('content')
 <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
     <div class="app-header header-shadow">
         <div class="app-header__logo">
@@ -465,5 +453,4 @@
         <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
     </div>
 </div>
-<script type="text/javascript" src="{{ URL::asset('assets/scripts') }}/main.js"></script></body>
-</html>
+@endsection
