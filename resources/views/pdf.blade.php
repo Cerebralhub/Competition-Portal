6c50cf41543cbd45e36865304711f67f23965152
richard4s <richardoluwo50@gmail.com>
2020-08-17 15:50:19 +0100

    pdf file

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -6,14 +6,17 @@
                 <div class="app-main__inner">
                     <div class="app-page-title">
                         <div class="page-title-wrapper" style="margin: auto;">
+
                             <div id="adobe-dc-view" style="height: 560px; width: 1000px; margin: auto;"></div>
                             <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
                             <script type="text/javascript">
+                                var pdfFile = @json(URL::asset('assets/docs/Merger Review Regulation Form.pdf'));
+                                // alert('Yooo' +  hello + ' lol');
                                 document.addEventListener("adobe_dc_view_sdk.ready", function(){
                                     var adobeDCView = new AdobeDC.View({clientId: "c3c67a26df6540cc89694b4b4942d562", divId: "adobe-dc-view"});
                                     adobeDCView.previewFile({
                                             content:{ location:
-                                                    { url: "https://fccpcmergers.ng/assets/docs/Merger%20Review%20Regulation%20Form.pdf"}},
+                                                    { url: pdfFile}},
                                             metaData:{fileName: "Merger Review Regulation Form.pdf"}
                                         },
                                         {
@@ -37,8 +40,8 @@
 
 <!-- <script type="text/javascript" src="{{ URL::asset('assets/scripts') }}/main.js"></script> -->
 <!-- <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script> -->
- <script type="text/javascript">
-    console.log({{ $hello }} );
+{{-- <script type="text/javascript">--}}
+{{--    console.log({{ $hello }} );--}}
 
- </script>
+{{-- </script>--}}
 @endsection
