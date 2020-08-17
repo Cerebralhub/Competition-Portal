3a028582a8225769691b1a1fdcf75c7c8906ebcc
richard4s <richardoluwo50@gmail.com>
2020-08-17 14:35:27 +0100

    document viewer

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,16 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="app-main">
+        <div class="app-main__outer">
                 <div class="app-main__inner">
                     <div class="app-page-title">
-                        <div class="page-title-wrapper">
-                            <p>Hello world</p>
-                        <div id="adobe-dc-view"></div>
+                        <div class="page-title-wrapper" style="margin: auto;">
+                            <div id="adobe-dc-view" style="height: 560px; width: 1000px; margin: auto;"></div>
+                            <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
+                            <script type="text/javascript">
+                                document.addEventListener("adobe_dc_view_sdk.ready", function(){
+                                    var adobeDCView = new AdobeDC.View({clientId: "249cd92da33946cbb239b623ad9cf874", divId: "adobe-dc-view"});
+                                    adobeDCView.previewFile({
+                                            content:{ location:
+                                                    { url: "https://fccpcmergers.ng/assets/docs/Merger%20Review%20Regulation%20Form.pdf"}},
+                                            metaData:{fileName: "Merger Review Regulation Form.pdf"}
+                                        },
+                                        {
+                                            embedMode: "SIZED_CONTAINER"
+                                        });
+                                });
+                            </script>
 
 
                             </div>
                           </div>
                     </div>
+
 
                 </div>
                 </div>
